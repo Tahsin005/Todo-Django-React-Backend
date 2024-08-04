5d181a93097b7bcde1111ada0045ad1dbd52ca58
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-08-04 23:39:29 +0600

    created serializer for todo model, view for the crud operations and routers for managing those crud operations

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from . serializers import TodoSerializer
+from . models import Todo
+from rest_framework import viewsets
 # Create your views here.
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
